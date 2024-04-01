5f5edc3e826406b7c4a2de383b4eb466000e516e
이상윤 <sangyoon@isang-yun-ui-Macmini.local>
2024-04-02 03:03:29 +0900

    백준 1000번 문제

---
+++ src/BackJoon/BackJoon1000.java
@@ -0,0 +1,21 @@
+package BackJoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BackJoon1000 {
+    public static void main(String[] args) throws IOException {
+        // 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+        // 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+        // 첫째 줄에 A+B를 출력한다.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b);
+
+    }
+}
